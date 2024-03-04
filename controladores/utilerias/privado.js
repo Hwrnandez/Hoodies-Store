dbc78c39d20dda57d65682206c828de19266e480
Diegoszj <20220196@ricaldone.edu.sv>
2024-03-04 09:50:11 -0600

    Pagina principal

--- controladores/utilerias/privado.js
+++ controladores/utilerias/privado.js
@@ -2,13 +2,13 @@ const header = document.querySelector('header')
 header.innerHTML =`
 <ul class="nav justify-content-center">
 <li class="nav-item">
-  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link active" aria-current="page" href="#">Productos</a>
 </li>
 <li class="nav-item">
-  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Categorias</a>
 </li>
 <li class="nav-item">
-  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Usuarios</a>
 </li>
 <li class="nav-item">
   <a class="nav-link disabled" aria-disabled="true">Disabled</a>
