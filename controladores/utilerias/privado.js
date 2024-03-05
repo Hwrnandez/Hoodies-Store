1e7de618fcefc2a012cb3ae934314d35b33c3099
carlosBurgos12 <20220686@ricaldone.edu.sv>
2024-03-05 15:39:56 -0600

    FINISHED

--- controladores/utilerias/privado.js
+++ controladores/utilerias/privado.js
@@ -1,23 +1,26 @@
 const header = document.querySelector('header')
 header.innerHTML =`<nav class="navbar bg-body-tertiary">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="../../vistas/privado/principal.html">
     <img src="../../recursos/img/Logo_hoodie.avif" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
     Hoodies Store
   </a>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Productos</a>
+      <a class="navbar-brand" href="../../vistas/privado/AdmProductos.html">Productos</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Categorias</a>
+            <a class="nav-link active" aria-current="page" href="../../vistas/privado/AdmCategoria">Categorias</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Usuarios</a>
+            <a class="nav-link" href="../../vistas/privado/AdmUsuarios">Usuarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="">Cerrar Sesion</a>
           </li>
           <li class="nav-item">
           </li>
