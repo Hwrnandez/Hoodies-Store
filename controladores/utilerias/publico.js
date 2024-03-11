44a06abba39518d72d94236796a2efbc08d773d9
Roditass <dominicasielmejiamendoza2020@gmail.com>
2024-03-11 15:17:24 -0600

    index

--- controladores/utilerias/publico.js
+++ controladores/utilerias/publico.js
@@ -5,15 +5,15 @@ header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light na
 <a class="navbar-brand" href="#">
 <img src="../../recursos/img/logo2.png" alt="" width="160" height="60">
 </a>
-  <a class="navbar-brand" href="#">Hoodie's Store</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+  
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-link active" aria-current="page" href="#">Hombre</a>
       <a class="nav-link active" href="#">Mujer</a>
-      <a class="nav-link active" href="#">Destacado</a></div>
+      <a class="nav-link active" href="#">Destacado</a>
+      <a <i class="bi bi-bag-fill"></i><a>
+      </div>
   </div>
 </div>
 </nav>`
