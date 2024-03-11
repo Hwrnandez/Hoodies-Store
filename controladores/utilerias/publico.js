6a2406693d9f05546aac95a3ab0b4b93833abb49
Roditass <dominicasielmejiamendoza2020@gmail.com>
2024-03-11 13:53:43 -0600

    index

---
+++ controladores/utilerias/publico.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('header')
+header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+<div class="container-fluid">
+ 
+<a class="navbar-brand" href="#">
+<img src="../../recursos/img/logo.png" alt="" width="160" height="60">
+</a>
+  <a class="navbar-brand" href="#">Hoodie's Store</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" aria-current="page" href="#">Hombre</a>
+      <a class="nav-link active" href="#">Mujer</a>
+      <a class="nav-link active" href="#">Destacado</a></div>
+  </div>
+</div>
+</nav>`
