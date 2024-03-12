5c501eb4fd5b69544989d37673478bf1c82ee24c
Roditass <dominicasielmejiamendoza2020@gmail.com>
2024-03-12 10:55:42 -0600

    footer

---
+++ controladores/utilerias/publico_footer.js
@@ -0,0 +1,7 @@
+const footer = document.querySelector('footer')
+footer.innerHTML = `<br>
+<br>
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    Hoodie´s Store
+  </div>`
