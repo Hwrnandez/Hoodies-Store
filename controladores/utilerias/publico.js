fe67cee4912328f0ae5221852dd55070cdc8d6d6
carlosBurgos12 <20220686@ricaldone.edu.sv>
2024-03-12 14:39:12 -0600

    index

--- controladores/utilerias/publico.js
+++ controladores/utilerias/publico.js
@@ -1,20 +1,17 @@
 const header = document.querySelector('header')
-header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
-<div class="container-fluid">
- 
-<a class="navbar-brand" href="#">
-<img src="../../recursos/img/logo.png" alt="" width="160" height="60">
-</a>
-
-  
-  <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" aria-current="page" href="#">Hombre</a>
-      <a class="nav-link active" href="#">Mujer</a>
-      <a class="nav-link active" href="#">Destacado</a>
-      <a class="nav-link active" href="index.html"><i class="bi bi-bag-fill"></i></a>
-
+header.innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../../recursos/img/logo.png" alt="" width="160" height="60">
+    </a>
+    <div class="collapse navbar-collapse justify-content-center menu" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Hombre</a>
+        <a class="nav-link active" href="#">Mujer</a>
+        <a class="nav-link active" href="#">Destacado</a>
+        <a class="nav-link active" href="index.html"><i class="bi bi-bag-fill"></i></a>
       </div>
+    </div>
   </div>
-</div>
-</nav>`
+</nav>` 
