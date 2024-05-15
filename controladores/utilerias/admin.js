c6c488bdcec9e0aaf24e8a2293697f7de26d8749
josue <20230357@ricaldone.edu.sv>
2024-05-15 11:55:37 -0600

    JUAREZ ACARREANDO CON MUCHOS COMITS

--- controladores/utilerias/admin.js
+++ controladores/utilerias/admin.js
@@ -33,7 +33,7 @@ const loadTemplate = async () => {
                     <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                         <div class="container">
                             <a class="navbar-brand" href="dashboard.html">
-                                <img src="../../resources/img/logo.png" alt="CoffeeShop" width="50">
+                                <img src="../../resources/img/logo.png" alt="Hodies Store" width="50">
                             </a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
