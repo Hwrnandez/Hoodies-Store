600c292b122ce42b772858b010c36ef21165bfc9
Hwrnandez <20220215@ricaldone.edu.sv>
2024-05-27 12:08:46 -0600

    pedidos

--- controlers/private/pedido.js
+++ controlers/private/pedido.js
@@ -1,4 +1,158 @@
+// Constantes para completar las rutas de la API.
+const PEDIDO_API = 'services/admin/pedidos.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('searchForm');
+// Constantes para establecer el contenido de la tabla.
+TABLE_BODY = document.getElementById('tableBody'),
+    ROWS_FOUND = document.getElementById('rowsFound');
+// Constantes para establecer los elementos del componente Modal.
+const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+    MODAL_TITLE = document.getElementById('modalTitle');
+// Constantes para establecer los elementos del formulario de guardar.
+const SAVE_FORM = document.getElementById('saveForm'),
+    ID_PEDIDO = document.getElementById('idPedido'),
+    CLIENTE_PEDIDO = document.getElementById('nombreCliente'),
+    DIRECCION_PEDIDO = document.getElementById('direccionPedido'),
+    FECHA_PEDIDO = document.getElementById('fechaPedido'),
+    ESTADO_PEDIDO = document.getElementById('estadoPedido');
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
+    // Se establece el título del contenido principal.
+    // MAIN_TITLE.textContent = 'Pedidos';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    fillTable();
 });
+
+// Método del evento para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método del evento para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    //(ID_PEDIDO.value) ? action = 'updateRow' : action = 'createRow';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const DATA = await fetchData(PEDIDO_API, 'updateRow', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se cierra la caja de diálogo.
+        SAVE_MODAL.hide();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, DATA.message, true);
+        ID_PEDIDO.value = null;
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+});
+
+//Función asíncrona para llenar la tabla con los registros disponibles.
+const fillTable = async (form = null) => {
+    // Se inicializa el contenido de la tabla.
+    ROWS_FOUND.textContent = '';
+    TABLE_BODY.innerHTML = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'searchRows' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const DATA = await fetchData(PEDIDO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            console.log(DATA.dataset);
+            // Se establece un icono para el estado del PEDIDO.
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TABLE_BODY.innerHTML += `
+                <tr>
+                    <td>${row.nombre_cliente}</td>
+                    <td>${row.direccion_pedido}</td
+                    <td>${row.fecha_regristo_pedido}</td>
+                    <td>${row.estado_pedido}</td>
+                    <td>
+                        <button type="button" class="btn btn-success" onclick="openUpdate(${row.id_pedido})">
+                            <i class="bi bi-info-circle"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        // Se muestra un mensaje de acuerdo con el resultado.
+        ROWS_FOUND.textContent = DATA.message;
+    } else {
+        sweetAlert(4, DATA.error, true);
+    }
+}
+
+
+//Función asíncrona para preparar el formulario al momento de actualizar un registro.
+const openUpdate = async (id) => {
+    // Se define un objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('idPedido', id);
+    // Petición para obtener los datos del registro solicitado.
+    const DATA = await fetchData(PEDIDO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se muestra la caja de diálogo con su título.
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar del pedido';
+        // Se prepara el formulario.
+        SAVE_FORM.reset();
+        CLIENTE_PEDIDO.disabled = true;
+        FECHA_PEDIDO.disabled = true;
+        DIRECCION_PEDIDO.disabled = true;
+        // Se inicializan los campos con los datos.
+        const ROW = DATA.dataset;
+        ID_PEDIDO.value = ROW.id_pedido;
+        CLIENTE_PEDIDO.value = ROW.cliente;
+        FECHA_PEDIDO.value = ROW.fecha;
+        DIRECCION_PEDIDO.value = ROW.direccion_pedido;
+        //SE COLOCA EL VALOR QUE VIENE DE LA BASE AL SELECT EN EL HTML
+        for (var i = 0; i < ESTADO_PEDIDO.options.length; i++) {
+            // Si el valor de la opción es igual al valor que quieres seleccionar
+            if (ESTADO_PEDIDO.options[i].value === ROW.estado_pedido) {
+                // Seleccionar la opción
+                ESTADO_PEDIDO.selectedIndex = i;
+                break; // Salir del bucle una vez seleccionada la opción
+            }
+        }
+    } else {
+        sweetAlert(2, DATA.error, false);
+    }
+}
+
+//Función asíncrona para eliminar un registro.
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea inactivar el PEDIDO de forma permanente?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idModelo', id);
+        // Petición para eliminar el registro seleccionado.
+        const DATA = await fetchData(PEDIDO_API, 'deleteRow', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra un mensaje de éxito.
+            await sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable(ESTADO_BUSQUEDA);
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    }
+}
