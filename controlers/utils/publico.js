1223fc52055945f72293c627e0370aefafee8a9d
Hwrnandez <20220215@ricaldone.edu.sv>
2024-05-28 13:19:45 -0600

    cambios de link

--- controlers/utils/publico.js
+++ controlers/utils/publico.js
@@ -1,38 +1,17 @@
 const header = document.querySelector('header')
-<<<<<<< HEAD:controlers/utils/publico.js
 header.innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="../../recursos/img/logo.png" alt="" width="160" height="60">
+      <img src="../../resources/img/logo.png" alt="" width="160" height="60">
     </a>
     <div class="collapse navbar-collapse justify-content-center menu" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="../../vistas/publico/hombresec.html">Hombre</a>
-        <a class="nav-link active" href="../../vistas/publico/mujeressec.html">Mujer</a>
-        <a class="nav-link active" href="../../vistas/publico/index.html">Destacado</a>
-        <a class="nav-link active" href="../../vistas/publico/carrito.html"><i class="bi bi-bag-fill"></i></a>
+        <a class="nav-link active" aria-current="page" href="../../views/public/hombresec.html">Hombre</a>
+        <a class="nav-link active" href="../../views/public/mujeressec.html">Mujer</a>
+        <a class="nav-link active" href="../../views/public/index.html">Destacado</a>
+        <a class="nav-link active" href="../../views/public/carrito.html"><i class="bi bi-bag-fill"></i></a>
       </div>
     </div>
   </div>
 </nav>` 
-=======
-header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
-<div class="container-fluid">
- 
-<a class="navbar-brand" href="#">
-<img src="../../recursos/img/logo2.png" alt="" width="160" height="60">
-</a>
-
-  
-  <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" aria-current="page" href="#">Hombre</a>
-      <a class="nav-link active" href="#">Mujer</a>
-      <a class="nav-link active" href="#">Destacado</a>
-      <a <i class="bi bi-bag-fill"></i><a>
-      </div>
-  </div>
-</div>
-</nav>`
->>>>>>> 44a06abba39518d72d94236796a2efbc08d773d9:controladores/utilerias/publico.js
