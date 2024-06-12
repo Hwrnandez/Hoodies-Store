e2ad47f2a1d31334a84e33da20e4ad3a49d8bf60
hwrnadez <20220215@ricaldone.edu.sv>
2024-06-12 09:43:28 -0600

    c

--- controlers/public/historial.js
+++ controlers/public/historial.js
@@ -19,7 +19,7 @@ let timeout_id;
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
-    loadTemplate5();
+    loadTemplate();
     // Se establece el título del contenido principal.
     // MAIN_TITLE.textContent = 'Historial de compras';
     // Llamada a la función para mostrar los productos del carrito de compras.
