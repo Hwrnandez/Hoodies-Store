34062e6b16d26a6cf0239823ef48e2e557f4e886
hwrnandez <20220215@ricaldone.edu.sv>
2024-07-22 12:08:30 -0600

    pedidos

--- controlers/private/principal.js
+++ controlers/private/principal.js
@@ -1,4 +1,5 @@
 const PRODUCTO_API = 'services/admin/productos.php';
+const PEDIDO_API = 'services/admin/pedidos.php';
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     graficoPastelCategorias();
     graficoBarrasMarcas ();
     graficopastelonCategorias();
+    graficobarraEstado();
 });
 
     /*
@@ -77,6 +79,28 @@ const graficopastelonCategorias = async () => {
         pieGraph('chart3', categorias, cantidades, 'Cantidad de productos por categoría');
     } else {
         document.getElementById('chart3').remove();
+        console.log(DATA.error);
+    }
+}
+
+const graficobarraEstado = async () => {
+    // Petición para obtener los datos del gráfico.
+    const DATA = await fetchData(PEDIDO_API, 'PorcentajeEstadoPedidos');
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
+    if (DATA.status) {
+        // Se declaran los arreglos para guardar los datos a gráficar.
+        let estado = [];
+        let cantidad = [];
+        // Se recorre el conjunto de registros fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se agregan los datos a los arreglos.
+            estado.push(row.ESTADO);
+            cantidad.push(row.PEDIDO);
+        });
+        // Llamada a la función para generar y mostrar un gráfico de pastel. Se encuentra en el archivo components.js
+        barGraph('chart4', estado, cantidad);
+    } else {
+        document.getElementById('chart4').remove();
         console.log(DATA.error);
     }
 }
